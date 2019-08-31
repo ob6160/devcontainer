0be29e28c537f46301783161c79bd629b0ce5d49
Zed <zed@192.168.1.104>
2019-08-31 18:53:59 +0100

    fix eoan

--- @devcontainer/eoan-xfce-novnc/index.js
+++ @devcontainer/eoan-xfce-novnc/index.js
@@ -2,7 +2,7 @@ const {DevcontainerGenerator} = require('@devcontainer/generator');
 const {writeFile} = require('fs').promises;
 
 const run = async () => {
-  const devGenerator = new DevcontainerGenerator('eoan');
+  const devGenerator = new DevcontainerGenerator('ubuntu:disco');
 
   devGenerator.setNodeVersion('current');
   devGenerator.setUpgraded();
